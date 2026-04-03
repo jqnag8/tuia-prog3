718c9503d89d262eb593e57bf9b4957704301bc8
Federico Winter <winterfedericoexe@gmail.com>
2026-04-03 20:22:56 -0300

    "Terminamos DFS"

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -17,12 +17,41 @@ class DepthFirstSearch:
         """
         # Initialize root node
         root = Node("", state=grid.initial, cost=0, parent=None, action=None)
-
+        
         # Initialize expanded with the empty dictionary
-        expanded = dict()
+        expanded : dict[tuple[int, int], bool] = dict()
+        expanded[root.state] = True
 
-        # Initialize frontier with the root node
-        # TODO Complete the rest!!
-        # ...
+        if grid.objective_test(root.state):
+            return Solution(root, expanded)
+        
+        frontier: StackFrontier = StackFrontier()
+        frontier.add(root)
+        
+        while not frontier.is_empty():
+            node : Node = frontier.remove()
+            possible_actions : list[str] = grid.actions(node.state)
+
+            if not possible_actions:            # Consultar redundancia
+                return NoSolution(expanded)
+
+            for action in possible_actions:
+                successor: tuple[int, int] = grid.result(node.state, action)
+
+                if successor in expanded:
+                    continue # Successor is already in expanded            
+                    
+                son: Node = Node(
+                    "",
+                    state = successor,
+                    cost = node.cost + grid.individual_cost(node.state, action),
+                    parent = node,
+                    action = action)
+
+                if grid.objective_test(successor):
+                    return Solution(son, expanded)
+
+                expanded[successor] = True
+                frontier.add(son)
 
         return NoSolution(expanded)
